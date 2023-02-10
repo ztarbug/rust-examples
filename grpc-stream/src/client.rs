437e9639289b0131dc48a790b4a66464331b8963
ztarbug <kronprinzfriedrich@gmx.de>
2023-02-10 09:52:48 +0100

    first attempt to use opencv sources for streaming

--- grpc-stream/src/client.rs
+++ grpc-stream/src/client.rs
@@ -9,6 +9,15 @@ use std::time::Duration;
 
 use std::sync::mpsc;
 
+use opencv::{
+    prelude::*,
+    videoio,
+    imgcodecs::*
+};
+
+use opencv::core::Vector;
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
@@ -24,24 +33,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = time::Instant::now();
     
     let outbound = async_stream::stream! {
-        let mut interval = time::interval(Duration::from_secs(1));
+        let mut interval = time::interval(Duration::from_millis(150));
+
+        let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap();
+        let mut frame = Mat::default();
 
         loop {
 
             if let Ok(()) = rx.try_recv() {
+                cam.release()
+                    .expect("closing cam didn't work");
                 break;
             };
 
-            let mut image_bytes: Vec<u8> = Vec::new();
-            image_bytes.push(1);
-            image_bytes.push(2);
-            image_bytes.push(3);
+            cam.read(&mut frame).unwrap();
+            
+            let mut jpg_buf: Vector<u8> = Vector::new();
+            let params: Vector<i32> = Vector::new();
+
+            match imencode(".jpg", &frame, &mut jpg_buf, &params) {
+                Ok(b) => {
+                    println!("Encode result {} with size {}", b, jpg_buf.len());
+                }
+                Err(e) => {
+                    println!("Converting to image didn't work: {e}");
+                }
+            }
 
             let time = interval.tick().await; //delay            
             let elapsed = time.duration_since(start);
 
             let i = ImageStream {
-                image: image_bytes            };
+                image: jpg_buf.as_slice().to_owned()
+            };
             println!("Time passed {:?}", elapsed);
 
             yield i;
--- grpc-stream/src/server.rs
+++ grpc-stream/src/server.rs
@@ -2,13 +2,25 @@ pub mod sample {
     tonic::include_proto!("sample");
 }
 
+use chrono::Local;
+
+use opencv::imgcodecs::IMREAD_ANYCOLOR;
+use opencv::imgcodecs::*;
+use opencv::core::Vector;
+
 use sample::{StreamResponse, ImageStream};
 use sample::sample_service_server::SampleServiceServer;
 use tonic::{transport::Server, Request, Response, Status, Streaming};
 use futures::StreamExt;
+use opencv::core::Mat;
 
-#[derive(Debug)]pub struct MyService {}
-#[tonic::async_trait]impl sample::sample_service_server::SampleService for MyService {
+#[derive(Debug)]
+pub struct MyService {
+}
+
+#[tonic::async_trait]
+impl sample::sample_service_server::SampleService for MyService {
+    
     async fn send_stream(
         &self,
         request: Request<Streaming<ImageStream>>,
@@ -17,16 +29,24 @@ use futures::StreamExt;
         let mut stream = request.into_inner();
         let mut stream_response = StreamResponse::default();
         stream_response.message = String::from("test response");
+
         while let Some(image) = stream.next().await {
-            let image = image.unwrap();
-            println!("received bytes {}", image.image.len());
-        } 
+            let image = image.unwrap().image;
+            let size = image.len();
+
+            let result = imdecode(&opencv::types::VectorOfu8::from_iter(image),IMREAD_ANYCOLOR).unwrap();
+            save_image(&result);
+
+            println!("received bytes {} and converted to {:?}", size, &result);
+        }
 
         Ok(Response::new(StreamResponse {message: String::from("test")}))
     }
 }
 
-#[tokio::main]async fn main()  -> Result<(), Box<dyn std::error::Error>> {
+
+#[tokio::main]
+async fn main()  -> Result<(), Box<dyn std::error::Error>> {
 
     //let addr = "[::1]:10000".parse().unwrap();    
     let addr = "127.0.0.1:10000".parse().unwrap();
@@ -36,5 +56,13 @@ use futures::StreamExt;
     let svc = SampleServiceServer::new(sample_service);
 
     Server::builder().add_service(svc).serve(addr).await?;
+
     Ok(())
+}
+
+fn save_image(image: &Mat) {
+    let filename = format!("tmp/received_image_{}.jpg", Local::now());
+    let params:Vector<i32> = Vector::new();
+    imwrite(&filename, &image, &params)
+        .expect("couldn't write file");
 }
